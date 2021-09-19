54704e788fae16ef8bc3ba36831621929b8f266b
Tomas Tillmann <obornyt@gitlab.mff.cuni.cz>
2021-09-19 19:21:15 +0200

    snake can grow now, added comments, minor code polishing

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 class Game {
+	// first time when the food is spawned on the scene
 	constructor() {
-		this.foodPosition = createVector(CANVAS_HEIGHT / 2,CANVAS_WIDTH / 2);
+		this.foodPosition = createVector(
+			this.findClosestMultiple(floor(random(CANVAS_WIDTH)), PIXEL_SIZE),
+			this.findClosestMultiple(floor(random(CANVAS_HEIGHT)), PIXEL_SIZE)
+			);
 	}
 
 	foodPosition;
@@ -20,19 +24,25 @@ class Game {
 	}
 
 	spawnFood() {
+		// if snake's head has the same position as food
 		if (snake.headBodyPart.position.x === this.foodPosition.x && snake.headBodyPart.position.y === this.foodPosition.y) {
-			console.log('food eaten');
 			this.foodPosition = createVector(
 					this.findClosestMultiple(floor(random(CANVAS_WIDTH)), PIXEL_SIZE),
 					this.findClosestMultiple(floor(random(CANVAS_HEIGHT)), PIXEL_SIZE)
 					);
+			
+			snake.grow();
 		}
 
+		// draws the food continously
+		// has to be done, because the scene is refreshed each frame (done in snake.move())
 		fill(240,100,100);
 		noStroke();
 		rect(this.foodPosition.x, this.foodPosition.y, PIXEL_SIZE, PIXEL_SIZE);
 	}
 
+	// returns the closest multiple of givenNumber
+	// used in order to spawn food on PIXEL_SIZE grid
 	findClosestMultiple(givenNumber, multipleOf) {
 		let closestMultiple;
 		let downFloor = ( givenNumber % multipleOf ) * multipleOf;
@@ -40,8 +50,6 @@ class Game {
 
 		abs( givenNumber - downFloor ) > abs( givenNumber - upFloor ) ? closestMultiple = upFloor : closestMultiple = downFloor;
 		return closestMultiple;
-
-
 	}
 }
 
@@ -59,15 +67,32 @@ class Snake {
 	constructor(bodyArr, speed) {
 		this.headBodyPart = bodyArr[0];
 		this.bodyArr = bodyArr;
+		this.tailBodyPart = bodyArr[bodyArr.length-1];
 	}
 
 	headBodyPart;
+	tailBodyPart;
 	bodyArr;
 
 	changeDirection(newDirection) {
 		// changes just the direction of the snake's head
+		// rest is done in snake.move()
 		let notAllowed = createVector((-1 * snake.headBodyPart.direction.x), (-1 * snake.headBodyPart.direction.y));
 		if (newDirection.x !== notAllowed.x && newDirection.y !== notAllowed.y) { this.headBodyPart.direction = newDirection; };
+	}
+
+	grow() {
+		// spawns a new tail on a correct position every time a food is eaten
+		let newTailBodyPart = new BodyPart(
+			createVector(
+				this.tailBodyPart.position.x + (-1 * this.tailBodyPart.direction.x * PIXEL_SIZE),
+				this.tailBodyPart.position.y + (-1 * this.tailBodyPart.direction.y * PIXEL_SIZE)
+			),
+			this.tailBodyPart.direction
+		);
+
+		this.bodyArr.push(newTailBodyPart);
+		this.tailBodyPart = newTailBodyPart;
 	}
 
 	move() {
@@ -91,7 +116,8 @@ class Snake {
 	show() {
 		// in order to delete unwanted bodyParts from the scene
 		createCanvas(CANVAS_HEIGHT, CANVAS_WIDTH);
-		background(51);
+
+		background(57,42,69);
 		noStroke();
 		fill(SNAKE_COLOR);
 		this.bodyArr.forEach(bodyPart => {
@@ -104,6 +130,10 @@ class Snake {
 let game;
 let snake;
 
+const gameStates = {DEAD: 0, ALIVE: 1};
+let gameState;
+
+// GLOBALS, CAN BE TWEAKED OPTIONALLY
 let CANVAS_HEIGHT;
 let CANVAS_WIDTH;
 let PIXEL_SIZE; 
@@ -112,11 +142,7 @@ let CANVAS_COLOR;
 let PIXEL_BORDERS_COLOR;
 let SNAKE_COLOR;
 
-const gameStates = {DEAD: 0, ALIVE: 1};
-let gameState;
-
 function setup() {
-	frameRate(10);
 	CANVAS_HEIGHT = 600;
 	CANVAS_WIDTH = 600;
 	PIXEL_SIZE = 20;
@@ -124,6 +150,9 @@ function setup() {
 	CANVAS_COLOR = color(51);
 	PIXEL_BORDERS_COLOR = color(156);
 	SNAKE_COLOR = color(255,240,254);
+
+
+	frameRate(10);
 	gameState = gameStates.ALIVE;
 
 	createCanvas(CANVAS_HEIGHT, CANVAS_WIDTH);
@@ -135,8 +164,6 @@ function setup() {
 		new BodyPart(createVector(CANVAS_WIDTH - 4*PIXEL_SIZE, CANVAS_HEIGHT / 2), createVector(-1,0)),
 		new BodyPart(createVector(CANVAS_WIDTH - 3*PIXEL_SIZE, CANVAS_HEIGHT / 2), createVector(-1,0)),
 		new BodyPart(createVector(CANVAS_WIDTH - 2*PIXEL_SIZE, CANVAS_HEIGHT / 2), createVector(-1,0)),
-		new BodyPart(createVector(CANVAS_WIDTH - PIXEL_SIZE, CANVAS_HEIGHT / 2), createVector(-1,0)),
-		new BodyPart(createVector(CANVAS_WIDTH,300), createVector(-1,0)),
 	];
 	snake = new Snake(snakeBody, 400);
 }
@@ -145,7 +172,7 @@ function draw() {
 	game.Run();
 }
 
-
+// unfortuntely, has to be run globally -> cannot be put inside a class for example
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
 		newDirection = createVector(0,-1);
@@ -162,6 +189,7 @@ function keyPressed() {
 	else if (keyCode === DOWN_ARROW) {
 		newDirection = createVector(0,1);
 	}
+
 	snake.changeDirection(newDirection);
 }
 
